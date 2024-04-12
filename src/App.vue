a6867539e5f404a183d03982f3bd724de9a19a28
Océane Robert <150052288+Oceane-Robert@users.noreply.github.com>
2024-04-12 10:57:16 +0200

    transition

--- src/App.vue
+++ src/App.vue
@@ -18,13 +18,20 @@ onErrorCaptured((err, instance, info) => {
   >
     menu
   </button>
-  <nav id="mainNav" v-show="menuIsOpen" >
-    <ul>
-      <li><a href="#">item 1</a></li>
-      <li><a href="#">item 2</a></li>
-      <li><a href="#">item 3</a></li>
-    </ul>
-  </nav>
+  <Transition
+      class="transition-transform duration-1000"
+      enter-from-class="-translate-x-full"
+      enter-to-class="translate-x-0"
+      leave-active-class="-translate-x-full"
+    >
+      <nav id="mainNav" v-show="menuIsOpen">
+        <ul>
+          <li><a href="#">item 1</a></li>
+          <li><a href="#">item 2</a></li>
+          <li><a href="#">item 3</a></li>
+        </ul>
+      </nav>
+    </Transition>
   </header>
   <RouterView v-slot="{ Component }">
     <Suspense>
